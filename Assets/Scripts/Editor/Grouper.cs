b6daa49e37c45c31030d483372a0c6e1eaadb4ce
baste <baste@rain-games.com>
2018-01-26 22:04:19 +0100

    group objects

---
+++ Assets/Scripts/Editor/Grouper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class Grouper {
+
+    [MenuItem("Commands/Group %g")]
+    public static void GroupSelected() {
+        var objects = Selection.gameObjects;
+        if (objects.Length == 0)
+            return;
+
+        var rootObject = new GameObject(objects[0].name + "_Group");
+        Undo.RegisterCreatedObjectUndo(rootObject, "Create group");
+
+        foreach (var gameObject in Selection.gameObjects) {
+            Undo.SetTransformParent(gameObject.transform, rootObject.transform, "Create group");
+        }
+
+        Selection.activeGameObject = rootObject;
+    }
+}
