1d07453bf9151735c9540130ce0fc14397f1fdd7
baste <baste@rain-games.com>
2018-01-26 23:51:13 +0100

    has eyes!

---
+++ Assets/Scripts/EyesController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EyesController : MonoBehaviour {
+
+    public static EyesController instance;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    public Canvas sightBlockOverlayCanvas;
+    public Camera player1Camera;
+    public Camera player2Camera;
+
+    public static void SetPlayerThatHasEyes(PlayerID player) {
+        instance.sightBlockOverlayCanvas.worldCamera = player == PlayerID.One ? instance.player2Camera : instance.player1Camera;
+    }
+
+}
