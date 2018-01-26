a74c62244316e06a8ea60c90093eda914ad77823
baste <baste@rain-games.com>
2018-01-26 21:06:59 +0100

    Can destroy stuff

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.HasComponent<Bullet>()) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class Extensions {
+    public static bool HasComponent<T>(this GameObject go) {
+        return go.GetComponent<T>() != null;
+    }
+
+    public static T GetComponent<T>(this Collision2D col) where T : Component {
+        return col.gameObject.GetComponent<T>();
+    }
+
+    public static bool HasComponent<T>(this Collision2D col) where T : Component {
+        return col.gameObject.HasComponent<T>();
+    }
+}
