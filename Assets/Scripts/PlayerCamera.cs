4d6310a1777cdb37f4d08c55da814977782e896c
baste <baste@rain-games.com>
2018-01-27 19:43:02 +0100

    Fixed Steffen Fureli

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -7,13 +7,13 @@ public class PlayerCamera : MonoBehaviour {
 
     Vector3 orgPos;
     Vector3 targetPos;
-	// Use this for initialization
-	void Start () {
+   // Use this for initialization
+   void Start () {
         playerMovement = transform.parent.GetComponent<PlayerMovement>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+   }
+   
+   // Update is called once per frame
+   void Update () {
 
         if (playerMovement.movementVector.magnitude != 0) {
         }
