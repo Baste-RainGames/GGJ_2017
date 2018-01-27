58e237f0064df3a868c2814c63ef10dccb4cce89
baste <baste@rain-games.com>
2018-01-27 17:07:40 +0100

    Ability indicator!

---
+++ Assets/AbilityIndicator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityIndicator : MonoBehaviour {
+
+    public Image gunBG;
+    public Image seeBG;
+    public Image blinkBG;
+
+    public void Set(Ability ability, bool value) {
+        Image image = null;
+        switch (ability) {
+            case Ability.Gun:
+                image = gunBG;
+                break;
+            case Ability.See:
+                image = seeBG;
+                break;
+            case Ability.Blink:
+                image = blinkBG;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(ability), ability, null);
+        }
+
+        image.color = value ? Color.green : Color.red;
+    }
+}
+
+public enum Ability {
+    Gun,
+    See,
+    Blink
+}
