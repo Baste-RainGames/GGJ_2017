b02c23b54f807aee5684797eb4913e59fa154bf4
baste <baste@rain-games.com>
2018-01-27 12:49:43 +0100

    walk on top of stuff

---
+++ Assets/WalkOnTopChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkOnTopChecker : MonoBehaviour {
+
+    private PlayerMovement playerMovement;
+
+    private HashSet<Collider2D> touching = new HashSet<Collider2D>();
+
+    private void Awake() {
+        playerMovement = GetComponentInParent<PlayerMovement>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        playerMovement.UpdateZPos(other.transform.position.z);
+        touching.Add(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        touching.Remove(other);
+        if (touching.Count == 0) {
+            playerMovement.ReturnToDefaultZPos();
+        }
+    }
+
+}
