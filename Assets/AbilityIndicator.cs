814a98ca3dda14b2efac74e206dbbe21b982c3fd
baste <baste@rain-games.com>
2018-01-28 16:04:49 +0100

    added icons!

--- Assets/AbilityIndicator.cs
+++ Assets/AbilityIndicator.cs
@@ -8,23 +8,29 @@ public class AbilityIndicator : MonoBehaviour {
     public Image seeBG;
     public Image blinkBG;
 
+    private Sprite hasGun, hasSee, hasBlink;
+    public Sprite noGun, noSee, noBlink;
+
+    void Awake() {
+        hasGun = gunBG.sprite;
+        hasSee = seeBG.sprite;
+        hasBlink = blinkBG.sprite;
+    }
+
     public void Set(Ability ability, bool value) {
-        Image image = null;
         switch (ability) {
             case Ability.Gun:
-                image = gunBG;
+                gunBG.sprite = value ? hasGun : noGun;
                 break;
             case Ability.See:
-                image = seeBG;
+                seeBG.sprite = value ? hasSee : noSee;
                 break;
             case Ability.Blink:
-                image = blinkBG;
+                blinkBG.sprite = value ? hasBlink : noBlink;
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(ability), ability, null);
         }
-
-        image.color = value ? Color.green : Color.red;
     }
 }
 
