e91567dbfc092f1c1481a0b30ed7e4822265d2fd
baste <baste@rain-games.com>
2018-01-28 14:40:22 +0100

    START WITH MOVIE

---
+++ Assets/LoadLevelAfterMovie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelAfterMovie : MonoBehaviour {
+
+    IEnumerator Start() {
+        yield return new WaitForSeconds(38f);
+        LoadNextLevel();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            LoadNextLevel();
+        }
+    }
+
+    private void LoadNextLevel() {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+}
--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using NUnit.Framework.Constraints;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
